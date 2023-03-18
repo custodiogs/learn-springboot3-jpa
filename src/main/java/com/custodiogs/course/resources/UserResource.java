4451483819a3963f556ef4839d9e7fece4555400
Gilmar Sousa <gilmar.custodio.sousa@gmail.com>
2023-03-18 18:18:20 -0300

    User entity and resource

---
+++ src/main/java/com/custodiogs/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.custodiogs.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.custodiogs.course.entities.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+
+		@GetMapping
+		public ResponseEntity<User> findAll(){
+			User u = new User(1L,"Maria","maria@gmail.com","999999","1231254");
+			return ResponseEntity.ok().body(u);
+		}
+}
