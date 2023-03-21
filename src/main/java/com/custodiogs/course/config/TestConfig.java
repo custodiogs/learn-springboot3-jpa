0ebb0fc87c632476a0579a5421da875cce111220
Gilmar Sousa <gilmar.custodio.sousa@gmail.com>
2023-03-20 22:59:14 -0300

    Payment, one-to-one association

--- src/main/java/com/custodiogs/course/config/TestConfig.java
+++ src/main/java/com/custodiogs/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.custodiogs.course.entities.Category;
 import com.custodiogs.course.entities.Order;
 import com.custodiogs.course.entities.OrderItem;
+import com.custodiogs.course.entities.Payment;
 import com.custodiogs.course.entities.Product;
 import com.custodiogs.course.entities.User;
 import com.custodiogs.course.entities.enums.OrderStatus;
@@ -71,6 +72,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-07-22T21:21:22Z"),o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 		
 	}
 }
