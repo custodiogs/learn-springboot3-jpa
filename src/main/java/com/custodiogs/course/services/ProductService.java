afa11bf7d2fc652c5489cb140e61fc5a7ebca468
Gilmar Sousa <gilmar.custodio.sousa@gmail.com>
2023-03-20 21:21:51 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/custodiogs/course/services/ProductService.java
+++ src/main/java/com/custodiogs/course/services/ProductService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.custodiogs.course.entities.Product;
-import com.custodiogs.course.entities.User;
 import com.custodiogs.course.repositories.ProductRepository;
 
 @Service
