eb92ad45d37e7b02eb4cc562c06f38cf37d28b8a
Gilmar Sousa <gilmar.custodio.sousa@gmail.com>
2023-03-21 00:21:02 -0300

    Exception handling - update

--- src/main/java/com/custodiogs/course/services/UserService.java
+++ src/main/java/com/custodiogs/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.custodiogs.course.repositories.UserRepository;
 import com.custodiogs.course.services.exceptions.DatabaseException;
 import com.custodiogs.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -43,9 +45,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User user) {
+		try {
 		User entity = userRepository.getReferenceById(id);
 		updateData(entity, user);
 		return userRepository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User user) {
